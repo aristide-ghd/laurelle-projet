f200c2fd5169ae2472674ef2b633d90eae539bd8
aristide-ghd <ariluxury03@gmail.com>
2024-10-13 15:24:09 +0100

    correction des listes deroulantes

--- dashbord/home.php
+++ dashbord/home.php
@@ -90,6 +90,6 @@
         <p class="mb-0 ">Copyright © 2024 Homechip's Laure | Tous droits réservés</p>
         <p class="mb-0 ">Design by: <a href="https://ari-luxury.com" class="text-white text-decoration-none">Ari-Luxury    </a></p>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 </body>
 </html>
