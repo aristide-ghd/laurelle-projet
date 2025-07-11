096592ff2de409c02ec49ca4605ec7b0ea73f52f
Aristide GBOHAÏDA <ariluxury03@gmail.com>
2025-07-11 17:01:33 +0100

    reprise du code a nouveau

--- sign_in.php
+++ sign_in.php
@@ -7,8 +7,8 @@ define("password","");
 $dsn = 'mysql:dbname='.database.';host='.hostname.';charset=utf8';    
 
 $bdd = new PDO($dsn, username, password);
-             // pour afficher les erreurs
+// pour afficher les erreurs
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-             // pour récupérer le résultat des requêtes SELECT sous forme de tableaux associatifs 
+// pour récupérer le résultat des requêtes SELECT sous forme de tableaux associatifs 
 $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC); 
 ?> 
